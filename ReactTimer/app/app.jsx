c36dc56284821b5b0d3ec0ed12d6fc939802e317
SimasRug <simonas.rugevicius@gmail.com>
2017-06-23 18:06:19 +0200

    Added a new React Timer app

---
+++ ReactTimer/app/app.jsx
@@ -0,0 +1,24 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+// var createReactClass = require('create-react-class');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var Main = require('Main');
+var Timer = require('Timer');
+var Countdown = require('Countdown');
+
+
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
+require('style!css!sass!applicationStyles');
+
+ReactDOM.render(
+ <Router history={hashHistory}>
+   <Route path="/" component={Main}>
+      <Route path="countdown" component={ Countdown }/>
+      <Route path="timer" component={ Countdown }/>
+      <IndexRoute component = { Timer }/>
+   </Route>
+ </Router>,
+  document.getElementById('app')
+);
---
+++ ReactTimer/app/components/Timer.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+var Clock = require('Clock');
+
+var Timer = (props) => {
+    return (
+        <div>
+            Timer comp
+        </div>
+    );
+
+};
+
+module.exports = Timer;
